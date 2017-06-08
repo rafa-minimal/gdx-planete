91a4d3e6753d6f58fe95d3b62e7a7be2612b922f
Rafał Golcz <rafal.golcz@gmail.com>
2017-06-08 22:01:31 +0200

    libGDX generated project

---
+++ android/src/com/minimal/planet/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.minimal.planet;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.minimal.planet.Planet;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Planet(), config);
+	}
+}
