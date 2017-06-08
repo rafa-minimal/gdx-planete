51e40b61984459515e9017463ebd179126b71c45
Rafał Golcz <rafal.golcz@gmail.com>
2017-06-08 22:10:30 +0200

    Dodany Kotlin do libGDX'a
    Wystarczyło w Android Studio dodać jeden plik Kotlina i IDE automatycznie
    wykrywa co trzeba zrobić tzn. dodać do gradle.build odpowiedni plugin i
    zależność od kotlin-stdlib-jre8

--- desktop/src/com/minimal/planet/desktop/DesktopLauncher.java
+++ desktop/src/com/minimal/planet/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.minimal.planet.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.minimal.planet.Planet;
+import com.minimal.planet.PlanetKt;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Planet(), config);
+		new LwjglApplication(new PlanetKt(), config);
 	}
 }
