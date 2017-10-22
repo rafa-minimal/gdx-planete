7c76899860749cab776cb55fd45004dee0283c6a
Rafał Golcz <rafal.golcz@gmail.com>
2017-10-22 21:32:23 +0200

    Fonts - zależne od rozdzielczości

--- core/src/com/minimal/arkanoid/wrap/WrapCtx.kt
+++ core/src/com/minimal/arkanoid/wrap/WrapCtx.kt
@@ -16,33 +16,32 @@ object WrapCtx {
 
     val camera = OrthographicCamera()
     val viewport = ScreenViewport()
-    //val viewport = FitViewport()
 
-    val font: BitmapFont
-    val bigFont: BitmapFont
+    var font: BitmapFont
+    var bigFont: BitmapFont
     val atlas: TextureAtlas
     val skin: Skin
 
     init {
         // On some devices it might start in portrait mode and then quickly rotate to landscape
-        val height = Math.min(Gdx.graphics.height, Gdx.graphics.width)
+        val dim = Math.min(Gdx.graphics.height, Gdx.graphics.width)
 
-        /*if (height <= 480) {
+        if (dim <= 480) {
             loadFonts(17, 26)
-            loadAtlas("mdpi")
-        } else if (height <= 480 * 1.5) {
+//            loadAtlas("mdpi")
+        } else if (dim <= 480 * 1.5) {
             loadFonts(22, 34)
-            loadAtlas("hdpi")
-        } else if (height <= 480 * 2) {
+//            loadAtlas("hdpi")
+        } else if (dim <= 480 * 2) {
             loadFonts(34, 51)
-            loadAtlas("xhdpi")
-        } else if (height <= 480 * 3) {
+//            loadAtlas("xhdpi")
+        } else if (dim <= 480 * 3) {
             loadFonts(44, 68)
-            loadAtlas("xxhdpi")
+//            loadAtlas("xxhdpi")
         } else {
             loadFonts(44, 68)
-            loadAtlas("xxxhdpi")
-        }*/
+//            loadAtlas("xxxhdpi")
+        }
 
         atlas = TextureAtlas(Gdx.files.internal("atlas.atlas"))
         font = BitmapFont(Gdx.files.internal("fonts/century-gothic-17.fnt"))
@@ -56,12 +55,12 @@ object WrapCtx {
 
     /*private fun loadAtlas(dir: String) {
         atlas = TextureAtlas(Gdx.files.internal(dir + "/asteroids.atlas"))
-    }
+    }*/
 
     private fun loadFonts(smallSize: Int, bigSize: Int) {
         font = BitmapFont(Gdx.files.internal("fonts/century-gothic-$smallSize.fnt"))
         bigFont = BitmapFont(Gdx.files.internal("fonts/century-gothic-$bigSize.fnt"))
-    }*/
+    }
 
     fun dispose() {
         batch.dispose()
