7ceae507f3d16b86dcc7ceb661186529e5e17a69
Rafał Golcz <rafal.golcz@gmail.com>
2017-11-21 23:57:43 +0100

    Odpalamy w najbardziej popularnych aspect ratio: 16:9  - 74% 15:9  - 17% 16:10 - 6%

---
+++ desktop/src/com/minimal/planet/desktop/DesktopLauncher.kt
@@ -0,0 +1,29 @@
+package com.minimal.planet.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.minimal.arkanoid.wrap.ArkanoidDebugGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        texturePacker(args)
+
+        val config = LwjglApplicationConfiguration()
+        if (args.contains("16:10")) {
+            config.width = 400
+            config.height = 640
+        } else if (args.contains("15:9") || args.contains("5:3")) {
+            config.width = 384
+            config.height = 640
+        } else {
+            // aspect ratio 16:9, most popular ~74%, see: https://hwstats.unity3d.com/mobile/display-android.html
+            config.width = 360
+            config.height = 640
+        }
+        //		new LwjglApplication(new Planet(), config);
+        //		new LwjglApplication(new ArkanoidGame(), config);
+        LwjglApplication(ArkanoidDebugGame(), config)
+        ParamsReloader("params.properties")
+    }
+}
